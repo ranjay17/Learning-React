08fe01bd3f52df032bf1e9da52d119c14ff58095
ranjay17 <sranjay15@gmail.com>
2025-07-13 13:52:34 +0530

    Add styling class to div in ExpenseItems component

---
+++ task-04-style-component/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ExpenseItem from './components/ExpenseItems';
+
+function App() {
+
+  return (
+    <>
+      <h1>Let's get Started</h1>
+      <ExpenseItem />
+    </>
+  );
+}
+
+export default App
---
+++ task-04-style-component/src/components/ExpenseItems.jsx
@@ -0,0 +1,18 @@
+// Write your code at relevant places in the code below:
+
+import "./ExpenseItems.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__location">Delhi</div>
+      <div>
+        <h2 className="expense-item__description">Book</h2>
+        <div className="expense-item__price">$10</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
