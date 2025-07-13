c2fce186fe6fd1767b33440a961a63d463b673a4
ranjay17 <sranjay15@gmail.com>
2025-07-13 13:29:17 +0530

    Update ExpenseItem component to display multiple expenses

---
+++ task-02-first-custom-component/first-custom-component/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ExpenseItems from './components/ExpenseItems.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Let's get Started</h1>
+      <ExpenseItems />
+    </>
+  )
+}
+
+export default App
