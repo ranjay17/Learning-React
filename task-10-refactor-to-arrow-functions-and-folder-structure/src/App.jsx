8b54121a709dbf76de9191414bf768173f880d00
ranjay17 <sranjay15@gmail.com>
2025-07-15 14:20:54 +0530

    refactor: Use arrow functions and organize components into subfolders

---
+++ task-10-refactor-to-arrow-functions-and-folder-structure/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Expenses from './components/Expenses/Expenses';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Let's get Started</h1>
+      <Expenses />
+    </>
+  )
+}
+
+export default App
